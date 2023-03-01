d43bc21dcbb1f3be4ffa9a2c7f6fb06e40d98bcc
Aman Giri <amangirikg@gmail.com>
2023-03-01 21:41:26 +0530

    added the function password

--- src/test/java/Locaters.java
+++ src/test/java/Locaters.java
@@ -13,11 +13,12 @@ public class Locaters {
 		WebDriverManager.edgedriver().setup();
 		WebDriver driver = new EdgeDriver();
 		String name = "Aman";
+		String password1 = getPassword(driver);
 		//DIFFERENT LOCATERS 
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys(name);
-		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.name("inputPassword")).sendKeys(password1);
 		driver.findElement(By.xpath("//button[@class='submit signInBtn']")).click();
 		Thread.sleep(2000);
 		String successmessage = driver.findElement(By.tagName("p")).getText();
@@ -29,5 +30,19 @@ public class Locaters {
 		driver.findElement(By.xpath("//button[text()='Log Out']")).click();
 		driver.close();
 		
+	}
+
+	public static String getPassword(WebDriver driver) throws InterruptedException{
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+
+		//Please use temporary password 'rahulshettyacademy' to Login.
+
+		String[] passwordArray = passwordText.split("'");
+		String password = passwordArray[1].split("'")[0];
+		return password;
 	}
 }
