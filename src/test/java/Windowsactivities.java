3517cb47e80110f873bb82c18b4657146408ac11
Aman Giri <amangirikg@gmail.com>
2023-03-03 00:47:25 +0530

    windows activities added

---
+++ src/test/java/Windowsactivities.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Windowsactivities {
+    public static void main(String[] args){
+
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+        driver.navigate().to("https://rahulshettyacademy.com/locatorspractice/");
+        driver.navigate().back();
+        
+
+
+    }
+}
